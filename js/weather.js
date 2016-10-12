4097c2df1d3b5b49137d7b9b8e3b668a07848f64
yuzai <a545905403@163.com>
2016-10-12 10:34:06 +0800

    fix the image fixed problem

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,14 @@
-var elem = document.getElementById('mySwipe');
-window.mySwipe = Swipe(elem, {
-   startSlide: 0,
-   speed: 2000,
-   auto: 0,
-   continuous: true,
-   disableScroll: false,
-   stopPropagation: false,
-  // callback: function(index, element) {},
-  // transitionEnd: function(index, element) {}
+
+window.mySwipe = new Swipe(document.getElementById('slider'), {
+  startSlide: 0,
+  speed: 400,
+  auto: 3000,
+  draggable: false,
+  continuous: true,
+  disableScroll: false,
+  stopPropagation: false,
+  callback: function(index, elem) {},
+  transitionEnd: function(index, elem) {}
 });
 var c=document.getElementById("line-weather");
 var ctx=c.getContext("2d");
